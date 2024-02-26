21cea589bb7ccfbb169a480d1a35ff47b2f459b6
jav1212 <haldrim377@gmail.com>
2024-02-26 12:03:06 -0400

    rail

--- dev_addons/gestion/__manifest__.py
+++ dev_addons/gestion/__manifest__.py
@@ -8,28 +8,38 @@
         Long description of module's purpose
     """,
     "author": "My Company",
+    "category": "Gestion Documental",
     "website": "https://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    "category": "Uncategorized",
     "version": "0.1",
     # any module necessary for this one to work correctly
-    "depends": ["base"],
+    "depends": ["base", "web", "mail", "hr"],
     # always loaded
     "data": [
+        "security/security.xml",
         "security/ir.model.access.csv",
         "views/views.xml",
         "views/pages/directorios.xml",
+        "views/pages/coordinadores.xml",
+        "views/pages/gerentes_jefes.xml",
         "views/pages/documentos.xml",
-        "views/pages/gestion.xml",
-        "views/pages/bandeja.xml",
         "views/pages/solicitud.xml",
         "views/pages/departamentos.xml",
+        "views/pages/empleados.xml",
+        "views/pages/respuestas.xml",
+        "views/pages/revisiones.xml",
         "views/templates.xml",
+        "mail/mail_solicitud_template.xml",
+        "mail/mail_revisado_template.xml",
+        "mail/mail_aprobado_template.xml",
+        "mail/mail_publicado_template.xml",
+        "reports/report.xml",
     ],
     # only loaded in demonstration mode
     "demo": [
         "demo/demo.xml",
     ],
+    "auto_install": True,
 }
--- dev_addons/gestion/models/models.py
+++ dev_addons/gestion/models/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo import _
 
 # TODO: ask about ids at models fields
 # TODO: ask about employes and how they are going to store specially if is User o Partner
@@ -16,93 +18,339 @@ class directorio(models.Model):
         help="Introduzca el nombre del directorio",
     )  # Nombre del directorio
     descripcion = fields.Text()  # Descripcion del directorio
-    documentos = fields.One2many("gestion.documento", "directorio")
+    documentos = fields.One2many(
+        string="Documentos", comodel_name="gestion.documento", inverse_name="directorio"
+    )  # Documentos del directorio
+    departamentos = fields.Many2many(
+        comodel_name="hr.department",
+        relation="directorio_departamento_rel",
+        column1="directorio_id",
+        column2="departamento_id",
+        string="Departamentos",
+        required=True,
+    )  # Departamentos que pueden acceder al departamento
+    last_update = fields.Datetime(
+        default=lambda self: fields.Datetime.now(), string="Fecha de creación"
+    )  # Fecha de emision
+    create_uid = fields.Many2one(
+        "res.users", string="Creado por", readonly=True
+    )  # Indica el creador del directorio
+
+    is_admin = fields.Boolean(compute="_compute_admin", default=True)
+
+    def _compute_admin(self):
+        for directorio in self:
+            directorio.is_admin = self.env.user.has_group(
+                "gestion.group_gestion_administrador"
+            )
+
+    is_visible = fields.Boolean(
+        compute="_compute_visible", default=True
+    )  # Verifica si es visible
+
+    @api.depends("departamentos", "is_admin")
+    def _compute_visible(self):
+        for directorio in self:
+            directorio.is_visible = (
+                self.env.user.department_id.id in directorio.departamentos.ids
+            ) or directorio.is_admin
 
 
 class documento(models.Model):
     _name = "gestion.documento"
     _description = "gestion.documento"
 
+    version = fields.Char()
+    comentarios_version = fields.Text()
+    code = fields.Char()
+    version = fields.Integer(default=1)
     name = fields.Char(
         string="Nombre",
         required=True,
         help="Introduzca el nombre del documento",
     )  # Nombre del documento
     descripcion = fields.Text()  # Descripcion del documento
-    directorio = fields.Many2one("gestion.directorio")
-    solicitud = fields.One2many("gestion.solicitud", "documento")
-    fileref = fields.Binary(string="Archivo")
-    is_editable = fields.Boolean(string="Editable", default=False)
+    directorio = fields.Many2one(
+        "gestion.directorio", required=True
+    )  # Directorio al que pertenece
+    solicitud = fields.One2many(
+        string="Solicitudes", comodel_name="gestion.solicitud", inverse_name="documento"
+    )  # Requerimientos de cambio para el documento
+    fileref = fields.Binary(string="Archivo")  # Archivo de formato pdf, word, ppt
+    departamentos = fields.Many2many(
+        comodel_name="hr.department",
+        relation="documento_departamento_rel",
+        column1="directorio_id",
+        column2="departamento_id",
+        string="Departamentos",
+        # compute="_compute_departments",
+    )
 
-    def emitir_solicitud_edicion(self):
-        return True
+    last_update = fields.Datetime(
+        default=lambda self: fields.Datetime.now(), string="Fecha de creación"
+    )
+
+    create_uid = fields.Many2one("res.users", string="Creado por", readonly=True)
+
+    is_admin = fields.Boolean(compute="_compute_admin", default=True)
+
+    def _compute_admin(self):
+        for documento in self:
+            documento.is_admin = self.env.user.has_group(
+                "gestion.group_gestion_administrador"
+            )
+
+    is_visible = fields.Boolean(
+        compute="_compute_visible", default=True
+    )  # Verifica si es visible
+
+    @api.depends("departamentos", "is_admin")
+    def _compute_visible(self):
+        for documento in self:
+            documento.is_visible = (
+                self.env.user.department_id.id in documento.departamentos.ids
+            ) or documento.is_admin
 
 
-class solicitud(models.Model):
+class Solicitud(models.Model):
     _name = "gestion.solicitud"
     _description = "gestion.solicitud"
 
+    state = fields.Selection(
+        [
+            ("borrador", "Borrador"),
+            ("solicitado", "Solicitado"),
+            ("elaboracion", "En elaboración"),
+            ("revision", "En revisión"),
+            ("revisado", "Revisado"),
+            ("aprobacion", "En aprobación"),
+            ("aprobado", "Aprobado"),
+            ("publicacion", "En publicación"),
+            ("publicado", "Publicado"),
+        ],
+        default="borrador",
+        string="Estatus de la solicitud",
+    )
+    state_color = fields.Char(
+        compute="_compute_state_color", store=False, default="red"
+    )
+
+    @api.depends("state")
+    def _compute_state_color(self):
+        for record in self:
+            if record.state == "borrador":
+                record.state_color = "red"
+            elif record.state == "solicitado":
+                record.state_color = "yellow"
+            elif record.state == "elaboracion":
+                record.state_color = "blue"
+            elif record.state == "revision":
+                record.state_color = "orange"
+            elif record.state == "revisado":
+                record.state_color = "green"
+            elif record.state == "aprobacion":
+                record.state_color = "purple"
+            elif record.state == "aprobado":
+                record.state_color = "lime"
+            elif record.state == "publicacion":
+                record.state_color = "pink"
+            elif record.state == "publicado":
+                record.state_color = "cyan"
+
     fecha_emision = fields.Datetime()
+    gerencia_solicitante = fields.Many2one(
+        "hr.department", readonly=True, default=lambda self: self.env.user.department_id
+    )
+
+    code = fields.Char(compute="_compute_code", default="SOL-NUEVA")
+    name = fields.Char(default=lambda self: self.code)
+
+    @api.depends()
+    def _compute_code(self):
+        for solicitud in self:
+            solicitud.code = f"SOL-{solicitud.id}"
+
+    email_from = fields.Char()
+    email_to = fields.Char()
+
+    # Requerimiento
+    requerimiento_selection = fields.Selection(
+        [
+            ("documento nuevo", "Documento nuevo"),
+            ("actualizacion", "Actualización"),
+            ("modificacion", "Modificación"),
+            ("procedimiento", "Procedimiento"),
+            ("instructivo", "Instructivo"),
+            ("formulario", "Formulario"),
+            ("manual", "Manual"),
+            ("políticas", "Políticas"),
+            ("otros", "Otros"),
+        ],
+        string="Requerimientos",
+    )
+    # Descripcion del requerimiento
+    otros_documentos_description = fields.Text()
+
+    # Origen de las solicitudes
+    origen_solicitud = fields.Selection(
+        [
+            ("auditoria externa", "Auditoria externa"),
+            ("auditoria interna", "Auditoria interna"),
+            ("modificaciones propias", "Modificaciones propias"),
+            (
+                "documentos vencidos por lista maestra",
+                "Documentos vencidos por Lista Maestra",
+            ),
+            ("integridad del sgc", "Integridad del SGC"),
+            ("cambio organizacionales", "Cambios organizacionales"),
+            (
+                "requisitos legales y reglamentarios",
+                "Requisitos Legales y Reglamentarios",
+            ),
+        ],
+        string="Origen de la solicitud",
+    )
+    requerimiento = fields.Text()
+
+    documento = fields.Many2one("gestion.documento", string="Documento asociado")
+    create_uid = fields.Many2one("res.users", string="Creado por", readonly=True)
+    reviewed_uid = fields.Many2one("res.users", string="Revisado por", readonly=True)
+    approved_uid = fields.Many2one("res.users", string="Aprobado por", readonly=True)
+    published_uid = fields.Many2one("res.users", string="Publicado por", readonly=True)
+    coordinador = fields.Many2one(
+        "res.users",
+        string="Coordinadores",
+    )
+
+    # fields para cuando no procede
+    descripcion_no_procede = fields.Text()
+
+    # fields para cuando no esta conforme con los cambios
+    descripcion_no_conforme_revision = fields.Text()
+
+    # fields para cuando no esta conforme la aprobacion
+    descripcion_no_conforme_aprobacion = fields.Text()
+
+    # fields para cuando se va a publicar
+    descripcion_publicacion = fields.Text()
+
+    fecha_revision = fields.Datetime()
+    fecha_aprobacion = fields.Datetime()
+    fecha_publicacion = fields.Datetime()
+
+    progress = fields.Integer(default=lambda self: 0, compute="_compute_progress")
+
+    @api.depends("state")
+    def _compute_progress(self):
+        for solicitud in self:
+            if solicitud.state == "borrador":
+                solicitud.progress = 0
+            elif solicitud.state == "solicitado":
+                solicitud.progress = 12.5
+            elif solicitud.state == "elaboracion":
+                solicitud.progress = 25
+            elif solicitud.state == "revision":
+                solicitud.progress = 37.5
+            elif solicitud.state == "revisado":
+                solicitud.progress = 50
+            elif solicitud.state == "aprobacion":
+                solicitud.progress = 62.5
+            elif solicitud.state == "aprobado":
+                solicitud.progress = 75
+            elif solicitud.state == "publicacion":
+                solicitud.progress = 87.5
+            elif solicitud.state == "publicado":
+                solicitud.progress = 100
+
+    def send_email(self):
+        self.state = "solicitado"
+        self.fecha_emision = fields.Datetime.now()
+        template = self.env.ref("gestion.mail_solicitud_template")
+        for rec in self:
+            template.send_mail(rec.id, force_send=True)
+
+    def no_procedente(self):
+        self.state = "borrador"
+        self.fecha_emision = False
+
+    def procedente(self):
+        self.state = "elaboracion"
+
+    def mandar_a_revision(self):
+        self.state = "revision"
+
+    def no_revisado(self):
+        self.state = "elaboracion"
+
+    def revisado(self):
+        self.state = "revisado"
+        self.fecha_revision = fields.Datetime.now()
+        self.reviewed_uid = self.env.user
+        template = self.env.ref("gestion.mail_revisado_template")
+        for rec in self:
+            template.send_mail(rec.id, force_send=True)
+
+    def mandar_a_aprobacion(self):
+        self.state = "aprobacion"
+
+    def no_aprobado(self):
+        self.state = "borrador"
+        self.fecha_emision = False
+        self.fecha_revision = False
+
+    def aprobado(self):
+        self.state = "aprobado"
+        self.fecha_aprobacion = fields.Datetime.now()
+        self.approved_uid = self.env.user
+        template = self.env.ref("gestion.mail_aprobado_template")
+        for rec in self:
+            template.send_mail(rec.id, force_send=True)
+
+    def mandar_a_publicacion(self):
+        self.state = "publicacion"
+
+    def publicado(self):
+        self.state = "publicado"
+        self.fecha_publicacion = fields.Datetime.now()
+        self.published_uid = self.env.user
+        template = self.env.ref("gestion.mail_publicado_template")
+        for rec in self:
+            template.send_mail(rec.id, force_send=True)
+
+    # TODO: preguntar hasta que punto se puede cancelar la solicitud
+    def cancelado(self):
+        if self.state == "solicitado":
+            # se envia un correo indicando pq no procede
+            self.state = "borrador"
+            self.fecha_emision = False
+        elif self.state == "revision":
+            # se envia un correo indicando pq no esta conforme con los cambios
+            self.state = "elaboracion"
+        elif self.state == "aprobacion":
+            # se envia un correo indicando pq no esta aprobacion conforme
+            self.state = "borrador"
+            self.fecha_emision = False
+            self.fecha_revision = False
+
+    def action_name(self):
+        pass
+
+
+class Revision(models.Model):
+    _name = "gestion.revision"
+    _description = "gestion.revision"
+
+    code = fields.Char()
+    fecha_emision = fields.Datetime(default=fields.Datetime.now())
+    create_uid = fields.Many2one("res.users", string="Creado por", readonly=True)
     descripcion = fields.Text()
-    estado = fields.Char()
-    fileref = fields.Binary(string="Archivo")
-    documento = fields.Many2one("gestion.documento", readonly=True)
 
 
-# TODO: this class can be abstract from database model User or Partner
-class empleado(models.Model):
-    _name = "res.users"
-    _inherit = "res.users"
-
-    departamento = fields.Many2one(
-        "gestion.departamento", string="Departamento"
-    )  # Departamento al que pertenece
-    es_coordinador_sgc = fields.Boolean()
-    es_gerente = fields.Boolean()
-    es_jefe_de_area = fields.Boolean()
-    es_director_de_operaciones = fields.Boolean()
-
-    # primer_nombre = fields.Char()  # Primer nombre del empleado
-    # primer_apellido = fields.Char()  # Primer apellido del empleado
-    # segundo_nombre = fields.Char()  # Segundo nombre del empleado
-    # segudo_apellido = fields.Char()  # Segundo apellido del empleado
-    # departamento = fields.Char()
-    # correo = fields.Char()  # Correo electronico del empleado
-
-
-# TODO: ask if they want to add more departament
-class departamento(models.Model):
-    _name = "gestion.departamento"
-    _description = "gestion.departamento"
-
-    name = fields.Char(
-        string="Nombre",
-        required=True,
-        help="Introduzca el nombre del departamento",
-    )  # Nombre del departamento
-    descripcion = fields.Text()  # Descripcion del documento
-
-
-# TODO: is just to have some nice charts
-class gestion(models.Model):
-    _name = "gestion.gestion"
-    _description = "gestion.gestion"
-
-    name = fields.Char()
-
-
-class bandeja(models.Model):
-    _name = "gestion.bandeja"
-    _description = "gestion.bandeja"
-
-    name = fields.Char()
-
-
-class respuesta(models.Model):
+class Respuesta(models.Model):
     _name = "gestion.respuesta"
     _description = "gestion.respuesta"
 
-    # TODO: generate a code id for this model
-
-    fecha_emision = fields.Datetime()
+    code = fields.Char()
+    fecha_emision = fields.Datetime(default=fields.Datetime.now())
+    create_uid = fields.Many2one("res.users", string="Creado por", readonly=True)
     descripcion = fields.Text()
